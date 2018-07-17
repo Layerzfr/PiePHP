044e0193fb0db829d2a1c83330f77525e25363ff
Baptiste Dumont <baptiste.dumont@epitech.eu>
2018-07-17 08:55:34 +0200

    léger correctif

--- index.php
+++ index.php
@@ -8,23 +8,7 @@
 	require_once(implode(DIRECTORY_SEPARATOR, ['Core', 'autoload.php']));
 	$app = new Core\Core();
 	$app->run();
-	//$test = new Core\ORM();
-	/*$test->create('users',  array(
-		'email' => 'test',
-		'password' => 'wow'
-	)); */
-	//$test->read('users', '56');
-	/* $test->update('users', 203, array(
-		'email' => 'ok',
-		'password' => 'test'
-	)); */ 
-	//$test->delete('users', '94');
-	/*$test->find('users', array(
-		"WHERE" => "id < 100",
-		'ORDER BY' => 'id ASC',
-         'LIMIT' => '10'
-	));*/
-	//$test = new \src\Model\UserModel(202);
+	
 	?>
 
 	<pre>
